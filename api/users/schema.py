63a159c414942187ebeafd03abad03892b183897
Naiyoma <naiyoma@mesozi.com>
2020-05-09 22:42:02 +0300

    query to fetch single objects

--- api/users/schema.py
+++ api/users/schema.py
@@ -13,10 +13,41 @@ class CategoryType(DjangoObjectType):
         model = Category
 
 class Query(graphene.ObjectType):
+    category = graphene.Field(CategoryType,
+                            id=graphene.Int(),
+                            name=graphene.String())
+    users = graphene.Field(UserType,
+                           id=graphene.Int(),
+                           name=graphene.String())
     all_users = graphene.List(UserType)
     all_categories = graphene.List(CategoryType)
 
-    def resolve_users(self, info, **Kwargs):
+    def resolve_category(self, info, **kwargs):
+
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Category.objects.get(pk=id)
+
+        if name is not None:
+            return Category.objects.get(name=name)
+
+        return None
+
+    def resolve_user(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return User.objects.get(pk=id)
+
+        if name is not None:
+            return User.objects.get(name=name)
+
+        return None
+
+    def resolve_all_users(self, info, **Kwargs):
         return User.objects.all()
 
     def resolve_all_categories(self, info, **Kwargs):
